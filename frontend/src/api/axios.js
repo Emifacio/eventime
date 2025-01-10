559874ec32801883bee83a6c8c683996df0ad3ea
Gabriel Emiliano Facio <127351922+Emifacio@users.noreply.github.com>
2025-01-09 23:21:46 -0300

    correccionEndpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL= import.meta.env.VITE_BACKEND || "https://eventime-production.up.railway.app/api";
+const baseURL= import.meta.env.VITE_BACKEND || "https://eventime-production.up.railway.app";
     
 const client = axios.create({
     baseURL,
