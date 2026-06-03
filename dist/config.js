cf9bcf3b24e080c66234bff8d1b4b8d11f308e93
Gabriel Emiliano Facio <127351922+Emifacio@users.noreply.github.com>
2026-06-03 17:47:17 -0300

    cleanup dist

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-export const PORT = Number(process.env.PORT) || 3000;
-export const JWT_SECRET = process.env.JWT_SECRET || 'xyz123';
-export const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
-export const ALLOWED_ORIGINS = [
-    'https://eventime.vercel.app',
-    'https://eventime-jqfzrmvt2-gabriel-emiliano-facios-projects.vercel.app'
-];
-export const WHITELIST = [ORIGIN, ...ALLOWED_ORIGINS];
